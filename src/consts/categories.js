d9d372bd104c4d280fa90ce505bc1a0805eed1c5
Oulimata <oulimataba2005@gmail.com>
2021-02-14 23:26:59 +0100

    page daccueil

---
+++ src/consts/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+    {id: '1', name: 'body', image: require('../assets/categories/body.jpg')},
+    {id: '2', name: 'jupe', image: require('../assets/categories/jupe.jpg')},
+    {id: '3', name: 'pantalon', image: require('../assets/categories/pantalon.jpg')},
+    {id: '4', name: 'robe', image: require('../assets/categories/robe.jpg')},
+  ];
+  
+  export default categories;
---
+++ src/consts/vetements.js
@@ -0,0 +1,28 @@
+const vetements = [
+    {
+      id: '1',
+      name: 'body dentelle',
+      price: '7,5',
+      image: require('../assets/bodydentelle.jpg'),
+    },
+    {
+      id: '2',
+      name: 'jupe courte',
+      price: '8.10',
+      image: require('../assets/jupecourte.jpg'),
+    },
+    {
+      id: '3',
+      name: 'taille haute',
+      price: '15.10',
+      image: require('../assets/taillehaute.jpg'),
+    },
+    {
+      id: '4',
+      name: 'robe de soirée',
+      price: '19.55',
+      image: require('../assets/robedesoiree.png'),
+    },
+  ];
+  
+  export default vetements;
